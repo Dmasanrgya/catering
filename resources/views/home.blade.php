6f735ce388d7e584c4aff104968b2fc75e77693b
Fahris12344 <farismhd255@gmail.com>
2024-08-26 08:49:57 +0700

    testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,8 @@
                         </div>
                     @endif
 
+                    ppppp
+
                     {{ __('You are logged in!') }}
                 </div>
             </div>
